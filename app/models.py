701c2b281d3a6eb1aa6e104844f23d16d32860b4
David Li <davidli012345@gmail.com>
2023-07-21 21:16:35 -0700

    fix: update threads data

--- app/models.py
+++ app/models.py
@@ -1,10 +1,34 @@
 from pydantic import BaseModel
-
+from typing import Optional, List
 class Item(BaseModel):
     name: str
     description: str | None = None
 
 
-class User(BaseModel):
+class UserIdentity(BaseModel):
     id: int
     username: str
+
+
+class ProfilePic(BaseModel):
+    height: int
+    url: str
+    width: int
+
+class User(BaseModel):
+    is_private: bool 
+    profile_pic_url: str
+    username: str
+    hd_profile_pic_versions: List[ProfilePic]
+    is_verified: bool
+    biography: Optional[str] = None
+    biography_with_entities: Optional[str] = None
+    follower_count: int
+    pk: str
+
+class UserData(BaseModel):
+    user: User
+
+class Response(BaseModel):
+    data: UserData
+    extensions: dict
--- app/routers/user_threads.py
+++ app/routers/user_threads.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, Depends
 
-from app.models import User
+from app.models import UserIdentity
 from app.services.parse_threads import get_id_from_username
 
 router = APIRouter(prefix="/threads")
 
-@router.get("/{username}")
-async def get_user(username: str) -> User:
+@router.get("/username/{username}")
+async def get_user(username: str) -> UserIdentity:
     """
     Get a user by their username.
 
@@ -17,5 +17,26 @@ async def get_user(username: str) -> User:
     - user: An instance of the User class representing the user with the given username.
     """
     user_id = get_id_from_username(username)
-    user = User(id=user_id, username=username)
+    user = UserIdentity(id=user_id, username=username)
     return user
+
+@router.get("/userid/{user_id}")
+def get_user_data(user_id: int) -> Response:
+  
+  response = threads.public_api.get_user(id=user_id)
+  
+  user_dict = response["data"]["userData"]["user"]
+  
+  profile_pics = [ProfilePic(**pic) for pic in user_dict["hd_profile_pic_versions"]]
+  
+  user = User(
+    hd_profile_pic_versions=profile_pics,
+    **user_dict
+  )
+  
+  data = UserData(user=user)
+
+  return Response(
+    data=data,
+    extensions=response["extensions"]
+  )
